377d9afa083ea46f54404d3b2289cce85bbf4864
Prem Bhalerao <114722173+premsbhalerao@users.noreply.github.com>
2024-01-13 13:53:32 +0530

    file adding

---
+++ swich_statement.java
@@ -0,0 +1,20 @@
+public class Main(){
+    public static void main(String[] args) {
+        int day = 3;
+        switch(day){
+        case 1:
+            System.out.println("Ruturaj");
+            break;
+        case 2:
+            System.out.println("Roshan");
+            break;
+        case 3:
+            System.out.println("Prem");
+            break;
+        case 4:
+            System.out.println("Rahul");
+            break;        
+        }
+        
+    }
+}
